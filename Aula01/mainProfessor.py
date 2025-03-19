6387294cebdf90d35d0adbde14e07c58b70bd2d8
ErikDuranti <erik.duranti@gmail.com>
2025-03-19 10:31:14 -0300

    Adição da função imprimir

--- Aula01/mainProfessor.py
+++ Aula01/mainProfessor.py
@@ -4,20 +4,23 @@ class Carro:
         self.ano = ano
         self.kilometragem = 0
         
-    def km(self):
+    def aumentarKm(self):
         if km > 0:
             self.__kilometragem += km
         
     
     def setKM(self, km):
-        if km > self._kilometragem:
+        if km > self.__kilometragem:
             self.__kilometragem = km
             
     def __str__(self):
         txt = "Modelo: " + self.modelo
-        txt += "\nAno: " + self.ano
+        txt += "\nAno: " + str(self.ano)
         txt += "\nKilometragem: " + str(self.__kilometragem)
         return txt
             
-x = Carrp("Doblo", 2025)
-print (x)
+    def imprimir(self):
+        print (self)
+        
+x = Carro("Doblo", 2025)
+x.imprimir()
